104c737a16c566047a0fb6002d5add6e6b3b8b40
wojea <annowojea@gmail.com>
2022-09-04 23:05:00 +0200

    add configurable server address

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 mod services;
+use std::env;
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let address = if args.len() > 1 {args[1].as_str()} else {"127.0.0.1"};
+    let port = if args.len() > 2 {args[2].clone().parse::<u16>().unwrap()} else {8080};
+    println!("{:?}", (address, port));
     HttpServer::new(|| {
         App::new()
             .service(services::calculateDisselUsageForDistance::calculateDisselUsageForDistance)
             .service(services::probabilityOfUnitInjectorFail::probabilityOfUnitInjectorFail)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((address, port))?
     .run()
     .await
 }
